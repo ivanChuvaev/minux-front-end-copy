ecb3a689bd4db12cd2f00bc55a6cf08a075be8a0
Shine <sabirovdima1@gmail.com>
2023-08-19 18:09:52 +0300

    fixes

--- front-end/src/pages/_settings/FlightSheet/model/FlightSheetList.tsx
+++ front-end/src/pages/_settings/FlightSheet/model/FlightSheetList.tsx
@@ -1,24 +1,28 @@
 import React, { HTMLProps } from 'react' 
 import styles from './FlightSheetList.module.scss'
 import arrowDown from 'shared/images/arrow-down.png'
+import threeDots from 'shared/images/three-dots.svg'
 
 export default function FlightSheetList(props: HTMLProps<HTMLDivElement>) {
     const data = [
         {id: 1, name: 'My RVN strategy', coinName: 'BTC', wallet: 'BYBIT RVN', pool: 'hiveon', miner: 'nbminer' },
-        {id: 1, name: 'My RVN strategy', coinName: 'BTC', wallet: 'BYBIT RVN', pool: 'hiveon', miner: 'nbminer' }
+        {id: 2, name: 'My RVN strategy', coinName: 'BTC', wallet: 'BYBIT RVN', pool: 'hiveon', miner: 'nbminer' }
     ]
   return (
     <div {...props} className={(props.className ?? "") + styles['wrapper']}>
         {data.map(item => (
-            <div className={styles['border-line']}>
-                <div key={item.id} className={styles['list-item']}>
-                    <p>{item.name}</p>
-                    <p>{item.coinName}</p>
-                    <p>{item.wallet}</p>
-                    <p>{item.pool}</p>
-                    <p>{item.miner}</p>
-                    <img src={arrowDown} alt='arrowDown'/>
-                </div> 
+            <div key={item.id} className={styles['flex-conteiner']}>
+                <div className={styles['border-line']}>
+                    <div className={styles['list-item']}>
+                        <p>{item.name}</p>
+                        <p>{item.coinName}</p>
+                        <p>{item.wallet}</p>
+                        <p>{item.pool}</p>
+                        <p>{item.miner}</p>
+                        <img src={arrowDown} alt='arrowDown'/>
+                    </div> 
+                </div>
+                <img src={threeDots} alt='options'/>
             </div>
         ))}
         
